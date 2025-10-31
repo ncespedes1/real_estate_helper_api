e676f7f08fa09e950aa3c407927a1ac7e8ded5c9
unknown <ncespedes.art@gmail.com>
2025-10-31 15:25:53 -0400

    user swagger done

--- config.py
+++ config.py
@@ -5,3 +5,12 @@ class DevelopmentConfig:
     DEBUG = True
     CACHE_TYPE =  "SimpleCache"
     CACHE_DEFAULT_TIMEOUT = 300
+
+class TestingConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    DEBUG = True
+    CACHE_TYPE = "SimpleCache"
+
+# class ProductionConfig:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
+#     CACHE_TYPE = "SimpleCache"
