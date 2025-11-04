0cefe2e609b7f1750abde09d1cc6997f0ae6267c
unknown <ncespedes.art@gmail.com>
2025-11-03 21:09:46 -0500

    deploy on render attempt

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from .blueprints.users import users_bp
 from .blueprints.county_data import county_data_bp
 from .blueprints.county_name_mapping import county_name_mapping_bp
 from flask_swagger_ui import get_swaggerui_blueprint
-# from flask_cors import CORS
+from flask_cors import CORS
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.yaml'
@@ -16,10 +16,10 @@ def create_app(config_name):
 
     app = Flask(__name__)
     app.config.from_object(f'config.{config_name}')
-    # CORS(app)
-
+    
     db.init_app(app)
     ma.init_app(app)
+    CORS(app)
 
     app.register_blueprint(users_bp, url_prefix='/users')
     app.register_blueprint(county_data_bp, url_prefix='/county_data')
--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class TestingConfig:
     DEBUG = True
     CACHE_TYPE = "SimpleCache"
 
-# class ProductionConfig:
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
-#     CACHE_TYPE = "SimpleCache"
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
+    CACHE_TYPE = "SimpleCache"
